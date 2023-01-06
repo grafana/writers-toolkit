7309ebb0fe6de57d95895137c96272783b194c59
Jack Baldry <jack.baldry@grafana.com>
2023-01-06 10:36:26 +0000

    Update initial contribution documentation to favor Podman
    Podman has the upside that containers can either be run as root or in
    rootless mode.
    
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- docs/docs.mk
+++ docs/docs.mk
@@ -5,9 +5,12 @@ export SHELLOPTS := pipefail:errexit
 MAKEFLAGS += --warn-undefined-variables
 MAKEFLAGS += --no-builtin-rule
 
-DOCS_IMAGE   = grafana/docs-base:latest
+DOCS_IMAGE   = docker.io/grafana/docs-base:latest
 DOCS_PROJECT = writers-toolkit
 DOCS_DIR     = sources
+
+# Support podman over Docker if it is available.
+PODMAN := $(shell if command -v podman &>/dev/null; then echo podman; else echo docker; fi)
 
 # This allows ports and base URL to be overridden, so services like ngrok.io can
 # be used to share a local running docs instances.
@@ -16,15 +19,15 @@ DOCS_LISTEN_PORT  = 3002
 DOCS_BASE_URL    ?= "localhost:$(DOCS_HOST_PORT)"
 
 HUGO_REFLINKSERRORLEVEL ?= WARNING
-DOCS_DOCKER_CONTAINER = $(DOCS_PROJECT)-docs
+DOCS_CONTAINER = $(DOCS_PROJECT)-docs
 
 .PHONY: docs-docker-rm
-docs-docker-rm:
-	docker rm -f $(DOCS_DOCKER_CONTAINER)
+docs-rm:
+	$(PODMAN) rm -f $(DOCS_CONTAINER)
 
 .PHONY: docs-pull
 docs-pull:
-	docker pull $(DOCS_IMAGE)
+	$(PODMAN) pull $(DOCS_IMAGE)
 
 .PHONY: docs
 docs: ## Serve documentation locally.
@@ -35,7 +38,7 @@ docs: docs-pull
 	@if [[ -z $${NON_INTERACTIVE} ]]; then \
 		read -p "Press a key to continue"; \
 	fi
-	docker run -it --name $(DOCS_DOCKER_CONTAINER) \
+	$(PODMAN) run -it --name $(DOCS_CONTAINER) \
 		-v $(CURDIR)/$(DOCS_DIR):/hugo/content/docs/$(DOCS_PROJECT)/:rw,z \
 		-e HUGO_REFLINKSERRORLEVEL=$(HUGO_REFLINKSERRORLEVEL) \
 		-p $(DOCS_HOST_PORT):$(DOCS_LISTEN_PORT) \

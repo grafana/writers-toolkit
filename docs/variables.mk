626b4f24b126b8252ba078b9a240402ea6ed8753
Jack Baldry <jack.baldry@grafana.com>
2023-04-05 11:33:58 +0100

    Remove unused variable
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- docs/variables.mk
+++ docs/variables.mk
@@ -1,6 +1,2 @@
 # List of projects to provide to the make-docs script.
 PROJECTS = writers-toolkit
-
-# The default version for grafana.com docs. Used by doc-validator for canonical checks.
-# Can be empty for unversioned projects.
-PRIMARY_PROJECT_DEFAULT_VERSION :=

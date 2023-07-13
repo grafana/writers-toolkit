232c11f47889e4aef355ba2958fe01070bc61c20
Jack Baldry <jack.baldry@grafana.com>
2023-07-13 10:41:21 +0100

    Use grafana/docs-base:latest again
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- docs/variables.mk
+++ docs/variables.mk
@@ -3,6 +3,3 @@ PROJECTS = writers-toolkit
 
 # Set the DOC_VALIDATOR_IMAGE to match the one defined in CI.
 export DOC_VALIDATOR_IMAGE := $(shell sed -En 's, *image: "(grafana/doc-validator.*)",\1,p' "$(shell git rev-parse --show-toplevel)/.github/workflows/test.yml")
-
-# Use alternative image until make-docs 3.0.0 is rolled out.
-export DOCS_IMAGE := grafana/docs-base:dbd975af06

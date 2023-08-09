e60c23290fa11734ddf447f4859cbf1908d6a585
Jack Baldry <jack.baldry@grafana.com>
2023-08-09 15:50:44 +0100

    Use Vale image defined in CI
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- docs/variables.mk
+++ docs/variables.mk
@@ -3,3 +3,6 @@ PROJECTS = writers-toolkit
 
 # Set the DOC_VALIDATOR_IMAGE to match the one defined in CI.
 export DOC_VALIDATOR_IMAGE := $(shell sed -En 's, *image: "(grafana/doc-validator.*)",\1,p' "$(shell git rev-parse --show-toplevel)/.github/workflows/test.yml")
+
+# Set the VALE_IMAGE to match the one defined in CI.
+export VALE_IMAGE := $(shell sed -En 's, *image: "(grafana/vale.*)",\1,p' "$(shell git rev-parse --show-toplevel)/.github/workflows/test.yml")

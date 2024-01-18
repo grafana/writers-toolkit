7eddece0a50d1fd611d949a35b1a5a9835e68ada
Jack Baldry <jack.baldry@grafana.com>
2024-01-18 15:57:31 +0000

    Update Vale and `reviewdog` to latest releases (#417)

--- docs/variables.mk
+++ docs/variables.mk
@@ -5,7 +5,7 @@ PROJECTS = writers-toolkit
 export DOC_VALIDATOR_IMAGE := $(shell sed -En 's, *image: "(grafana/doc-validator.*)",\1,p' "$(shell git rev-parse --show-toplevel)/.github/workflows/validate-documentation.yml")
 
 # Set the VALE_IMAGE to match the one defined in CI.
-export VALE_IMAGE := $(shell sed -En 's, *image: "(grafana/vale.*)",\1,p' "$(shell git rev-parse --show-toplevel)/.github/workflows/validate-documentation.yml")
+export VALE_IMAGE := $(shell sed -En 's, *image: (grafana/vale.*),\1,p' "$(shell git rev-parse --show-toplevel)/.github/workflows/validate-documentation.yml")
 
 # Skip some doc-validator checks.
 export DOC_VALIDATOR_SKIP_CHECKS := $(shell sed -En "s, *'--skip-checks=(.+)',\1,p" "$(shell git rev-parse --show-toplevel)/.github/workflows/validate-documentation.yml")

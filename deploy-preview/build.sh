b4f290bc27cd8a14073e2d25ee305d6dbc50128f
Robby Milo <robbymilo@fastmail.com>
2024-12-04 10:47:42 +0100

    use envs

--- deploy-preview/build.sh
+++ deploy-preview/build.sh
@@ -2,7 +2,7 @@
 
 docker run \
   -v "${PWD}/dist:/hugo/dist" \
-  -v "${PWD}/${{ inputs.source_directory }}:/hugo/${{ inputs.website_directory }}" \
+  -v "${PWD}/${source_directory}:/hugo/${website_directory}" \
   --rm grafana/docs-base:latest \
   /bin/bash \
     -c 'HUGO_SSI=false hugo --environment=docs --destination=dist/ --baseURL= --minify'

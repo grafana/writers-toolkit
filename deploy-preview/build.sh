d0d4e6323244ab469db3dd813b48786ae3a5bfc3
Robby Milo <robbymilo@fastmail.com>
2024-12-04 11:21:26 +0100

    fix file

--- deploy-preview/build.sh
+++ deploy-preview/build.sh
@@ -2,7 +2,7 @@
 
 docker run \
   -v "${PWD}/dist:/hugo/dist" \
-  -v "${PWD}/${source_directory}:/hugo/${website_directory}" \
+  -v "${source_directory}:/hugo/${website_directory}" \
   -e index_file \
   -e repo \
   -e website_directory \
@@ -10,13 +10,15 @@ docker run \
   /bin/bash \
     -c '
 if [[ "${index_file}" == "true" ]]; then
-  mkdir -p "/hugo/${website_directory}/content/docs/${repo}" && \
-  cat > "/hugo/${website_directory}/content/docs/${repo}/_index.md" <<EOF
+  echo "Creating custom _index.md" && \
+  cat > "/hugo/content/docs/${repo}/_index.md" <<EOF
+---
 type: redirect
 redirectURL: /docs/${repo}/latest/
 versioned: true
+---
 EOF
 fi
-cat "/hugo/${website_directory}/content/docs/${repo}/_index.md"
+cat "/hugo/content/docs/${repo}/_index.md"
 HUGO_SSI=false hugo --environment=docs --destination=dist/ --baseURL= --minify
 '

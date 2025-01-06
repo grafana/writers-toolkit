7746c2e7f9d313fcef1dd976b2ed3e80afca07be
Jack Baldry <jack.baldry@grafana.com>
2025-01-06 13:06:47 +0000

    Replace individual inputs with JSON string array of inputs
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- deploy-preview/build.sh
+++ deploy-preview/build.sh
@@ -1,24 +1,73 @@
 #!/usr/bin/env bash
 
-docker run \
-  -v "${PWD}/dist:/hugo/dist" \
-  -v "${PWD}/${SOURCE_DIRECTORY}:/hugo/${WEBSITE_DIRECTORY}" \
-  -e INDEX_FILE \
-  -e REPO \
-  -e WEBSITE_DIRECTORY \
-  --rm grafana/docs-base:latest \
-  /bin/bash \
-    -c '
-if [[ "${INDEX_FILE}" == "true" ]]; then
-  echo "Creating custom _index.md" && \
-  cat > "/hugo/content/docs/${REPO}/_index.md" <<EOF
+set -euf -o pipefail
+
+# All input comes from environment variables that are capitalized by convention in this script.
+SOURCES="${SOURCES:-[]}"
+
+if ! jq -e . <<<"${SOURCES}" >/dev/null; then
+  echo "SOURCES environment variable is not valid JSON"
+fi
+
+tempfile="$(mktemp -t deploy-preview.XXX)"
+# Pull out the relevant fields from the JSON because the grafana/docs-base image doesn't have jq.
+index_files=()
+while read -r index_file relative_prefix; do
+  if [[ "${index_file}" != 'null' ]]; then
+    index_files+=("${index_file}:${relative_prefix}")
+  fi
+done < <(jq -r '.[] | "\(.index_file) \(.relative_prefix)"' <<<"${SOURCES}")
+cat <<EOSCRIPT >"${tempfile}"
+#!/usr/bin/env bash
+
+# Create an index file to redirect a project root to the correct versioned URL.
+for pair in ${index_files[@]}; do
+  IFS=':' read -r index_file relative_prefix <<<"\${pair}"
+  dst="/hugo/\${index_file}"
+  parent="\${dst%/*}"
+  echo "Creating custom index: \${dst} -> \${relative_prefix}"
+
+  title="\${dst%/*}"
+  while [[ -n "\${parent}" ]]; do
+    if [[ ! -f "\${parent}/_index.md" ]]; then
+       cat > "\${parent}/_index.md" <<EOPARENT
+---
+title: \${title}
+---
+
+# \${title}
+
+{{< section >}}
+EOPARENT
+    fi
+    parent="\${parent%/*}"
+  done
+
+  cat > "\${dst}" <<EOINDEX
 ---
 type: redirect
-redirectURL: /docs/${REPO}/latest/
+redirectURL: \${relative_prefix}
 versioned: true
 ---
-EOF
-fi
-cat "/hugo/content/docs/${REPO}/_index.md"
+EOINDEX
+  cat "\${dst}"
+done
+
 HUGO_SSI=false hugo --environment=docs --destination=dist/ --baseURL= --minify
-'
+EOSCRIPT
+chmod +x "${tempfile}"
+
+volumes=("--volume=${PWD}/dist:/hugo/dist" "--volume=${tempfile}:/entrypoint:z")
+while read -r source_directory website_directory; do
+  volumes+=("--volume=${PWD}/${source_directory}:/hugo/${website_directory}:z")
+done < <(jq -r '.[] | "\(.source_directory) \(.website_directory)"' <<<"${SOURCES}")
+
+IFS='' read -r cmd <<EOF
+docker run \
+  ${volumes[@]} \
+  --rm grafana/docs-base:latest \
+  /entrypoint
+EOF
+
+echo "${cmd}"
+${cmd}
